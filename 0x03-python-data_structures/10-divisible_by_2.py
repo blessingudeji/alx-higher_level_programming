9b9b1f899510efe93a1099ea7e069bc63616b42d
Blessing <budeji@gmail.com>
2023-06-12 18:51:28 -0700

    Fixing typo error on line 6 for 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     newlist = []
     for a in range(len(my_list)):
-        if my_list[n] % 2 == 0:
+        if my_list[a] % 2 == 0:
             newlist.append(True)
         else:
             newlist.append(False)
