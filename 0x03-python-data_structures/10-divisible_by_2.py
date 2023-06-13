c0f3fee811e910c0879c9aa731d437373061db9d
Blessing <budeji@gmail.com>
2023-06-12 18:49:16 -0700

    Commit for 10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    newlist = []
+    for a in range(len(my_list)):
+        if my_list[n] % 2 == 0:
+            newlist.append(True)
+        else:
+            newlist.append(False)
+    return newlist
