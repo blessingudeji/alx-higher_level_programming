5448f5ac1143a201631afffc6786f00246ce99b8
Blessing <budeji@gmail.com>
2023-06-12 17:52:57 -0700

    Second commit for 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    list_length = len(my_list)
+    mylist_length = len(my_list)
     if idx < 0:
         return (None)
-    if idx > (list_length - 1):
+    if idx > (mylist_length - 1):
         return (None)
 
     return (my_list[idx])
