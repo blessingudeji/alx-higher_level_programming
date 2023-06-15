c38bbe2d1399e1808bd1d373400de05813ce800b
Blessing <budeji@gmail.com>
2023-06-14 19:05:25 -0700

    Granting permissions for 0-main.py

--- 0x04-python-more_data_structures/0-main.py
+++ 0x04-python-more_data_structures/0-main.py
@@ -10,7 +10,3 @@ matrix = [
 new_matrix = square_matrix_simple(matrix)
 print(new_matrix)
 print(matrix)
-
-guillaume@ubuntu:~/0x04$ ./0-main.py
-[[1, 4, 9], [16, 25, 36], [49, 64, 81]]
-[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
