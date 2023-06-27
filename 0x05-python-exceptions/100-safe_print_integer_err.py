c45aa6516ba6a3bc4c7b7ae2b0706920bbe03da4
Blessing <budeji@gmail.com>
2023-06-26 19:31:47 -0700

    Commit for Safe print integer error

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    from sys import stderr
+    try:
+        print("{:d}".format(value))
+    except Exception as i:
+        stderr.write("Exception: {}\n".format(i))
+        return (False)
+    else:
+        return (True)
