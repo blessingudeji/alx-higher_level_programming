daae1f0c4edb94d90e97be7c005dc9d6bc93a0d6
Blessing <budeji@gmail.com>
2023-06-28 15:02:10 -0700

    Fixing checker errors for 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+""" python class"""
+
 
 class Square():
     """creates square class"""
 
     def __init__(self, size):
-        """
-	initializes the class
+        """ initializes the class
 
         args:
            size: size of the square
