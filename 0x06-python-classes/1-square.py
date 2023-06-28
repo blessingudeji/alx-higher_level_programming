274a9b2d7d4ee932f658e3417cc5e37f9990812a
Blessing <budeji@gmail.com>
2023-06-28 13:38:58 -0700

    Commit for 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class Square():
+    """creates square class"""
+
+    def __init__(self, size):
+        """
+	initializes the class
+
+        args:
+           size: size of the square
+        """
+        self.__size = size
