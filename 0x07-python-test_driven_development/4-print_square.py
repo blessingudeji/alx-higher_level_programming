f4660da0f9749840dfe43b537c800df580b913a8
Blessing Udeji <budeji@gmail.com>
2023-07-08 04:27:32 +0100

    fixing errors

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''The print square module'''
+
+
+def print_square(size):
+    if size == 0:
+        return
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    print('\n'.join('#' * size for x in range(size)))
