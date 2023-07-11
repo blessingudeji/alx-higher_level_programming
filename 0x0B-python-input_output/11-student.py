940a6cc4a2712aa4e6ecf11dbaee644bf7fdd08f
Blessing Udeji <budeji@gmail.com>
2023-07-11 20:57:14 +0100

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""student class"""
+
+
+class Student:
+    """class representation"""
+    def __init__(self, first_name, last_name, age):
+        """initializes the class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """prints __dict__"""
+        if(attrs and isinstance(attrs, list) and
+           all(isinstance(x, str) for x in attrs)):
+            return ({x: y for x, y in self.__dict__.items() if x in attrs})
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces all attributes"""
+        if(json):
+            self.__dict__ = json
