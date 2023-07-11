80276ce6c2753f99dfcdc0f3eae280504c919a80
Blessing Udeji <budeji@gmail.com>
2023-07-11 04:50:22 +0100

    Adds attribute to

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""add_attribute function"""
+
+
+def add_attribute(obj, attr, value):
+    """adds new attribute yo the object"""
+    if('__slots__' in dir(obj) or '__dict__' not in dir(obj) or
+       hasattr(obj, attr)):
+        raise TypeError("can't add new attribute")
+    setattr(obj, attr, value)
