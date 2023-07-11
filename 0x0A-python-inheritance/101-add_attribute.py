b60fc304accdee627549ce0d90bb9eeea036797c
Blessing Udeji <budeji@gmail.com>
2023-07-11 09:58:49 +0100

    fixing errors

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,7 +3,7 @@
 
 
 def add_attribute(obj, attr, value):
-    """adds new attribute yo the object"""
+    """adds new attribute to the object"""
     if('__slots__' in dir(obj) or '__dict__' not in dir(obj) or
        hasattr(obj, attr)):
         raise TypeError("can't add new attribute")
